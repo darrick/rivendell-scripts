f77c8184dfe4fbcaa5b43e61c43529b6b3830e16
Alastair Ling <tech@edgeradio.org.au>
2020-03-30 01:42:02 +1100

    Curl Install Script Rewrite
    The default new install script has been updated to perform package checks, repository check, database check, audio storage check and existing configuration check.

--- install.sh
+++ install.sh
@@ -2,98 +2,101 @@
 
 clear
 
-echo
-echo Rivendell complete install script for Raspbian/Debian
-echo More information and Raspberry Pi images visit https://github.com/edgeradio993fm
-echo More information and source code at rivendellaudio.org
-echo
+echo ; echo "Rivendell complete install script for Raspbian/Debian" ; echo "More information and Raspberry Pi images visit https://github.com/edgeradio993fm" ; echo "More information and source code at rivendellaudio.org" ; echo
 
-echo We need to download and install a bunch of packages before Rivendell. This process could take a while...
-echo
+if dpkg -l | grep -qw rivendell
+  then
+    echo "Package rivendell is already installed" ; echo "Update your installation using: sudo apt update -y && sudo apt upgrade -y"
+  else
 
-echo Adding Rivendell on Raspberry Pi repo to your system...
-echo
+echo ; echo "We need to download and install a bunch of packages before Rivendell. This process could take a while..." ; echo
 
-wget -qO - https://7edg.org/repo/rivendellpi.key | sudo apt-key add -
+echo ; echo "Adding Rivendell on Raspberry Pi repository to your system..." ; echo
 
-sudo echo "deb https://7edg.org/repo buster main" |sudo tee -a /etc/apt/sources.list
+if grep -R -q "deb https://7edg.org/repo buster main" "/etc/apt/sources.list"
+  then 
+    echo "Reopsitory already added. Skipping..."
+  else
+    wget -qO - https://7edg.org/repo/rivendellpi.key | sudo apt-key add - && sudo echo "deb https://7edg.org/repo buster main" |sudo tee -a /etc/apt/sources.list
+fi
 
-echo
-echo Making sure your package database is up to date...
-echo
+echo ; echo "Making sure your package database is up to date..." ; echo
 
 sudo apt update -y && sudo apt upgrade -y
 
-echo
-echo Installing build tools...
-echo
+echo ; echo "Installing build tools..." ; echo
 
 sudo apt install -y libtool m4 automake pkg-config make gcc g++ autofs
 
-echo
-echo Installing Rivendell dependencies...
-echo
+echo ; echo "Installing Rivendell dependencies..." ; echo
 
 sudo apt install -y libssh-dev libsamplerate0-dev libsndfile1-dev libcdparanoia-dev libid3-3.8.3-dev libcurl4-openssl-dev libpam0g-dev libsoundtouch-dev libasound2-dev libflac++-dev libmp4v2-dev libmad0-dev libtwolame-dev libmp3lame-dev libfaad-dev libqt4-dev libqt4-sql-mysql libexpat1-dev libtag1-dev libjack-jackd2-dev python3-mysqldb libmusicbrainz5-dev libcoverart-dev libcoverart1 libcoverartcc1v5 libdiscid0 libdiscid-dev
 
 export PATH=/sbin:$PATH
 
-echo
-echo Installing and configuring Apache2...
-echo
+echo ; echo "Installing and configuring Apache2..." ; echo
 
-sudo apt install -y apache2
+if dpkg -l | grep -qw apache2
+  then
+    echo "Package apache2 is already installed. Skipping..." ; echo
+  else
+	sudo apt install -y apache2
+fi
 
 sudo a2enmod cgid
-
 sudo systemctl restart apache2
 
-echo
-echo Installing and configuring MariaDB...
-echo
+echo ; echo "Installing and configuring MariaDB..." ; echo
 
-sudo apt install -y mariadb-server
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
+if dpkg -l | grep -qw mariadb-server
+  then
+    echo "Package mariadb-server is already installed. Skipping..." ; echo
+  else
+	sudo apt install -y mariadb-server
+	sudo systemctl start mariadb
+	sudo systemctl enable mariadb
+fi
 
-sudo mysql -e "create database Rivendell;"
-sudo mysql -e "grant Select, Insert, Update, Delete, Create, Drop, References, Index, Alter, Create Temporary Tables, Lock Tables on Rivendell.* to rduser@'%' identified by 'letmein';"
 
-echo
-echo Making audio storage...
-echo
+echo "Making audio storage..." ; echo
 
-sudo adduser --system --group --home=/var/snd rivendell
-sudo adduser $SUDO_USER rivendell
-sudo chmod g+w /var/snd
+if [ -d /var/snd ]
+  then
+    echo "Audio storage already exists. Skipping..."
+  else
+    sudo adduser --system --group --home=/var/snd rivendell ; sudo adduser $SUDO_USER rivendell ; sudo chmod g+w /var/snd
+fi
 
-cd /home/$SUDO_USER
-
-echo
-echo Downloading Rivendell...
-echo
+echo ; echo "Downloading Rivendell..." ; echo
 
 sudo apt install rivendell
 
 sudo ldconfig
 
-echo
-echo Downloading default configuration...
-echo
+echo ; echo "Downloading default configuration..." ; echo
 
-sudo wget -O /etc/rd.conf https://7edg.org/rdinstall/rd.conf
+if [ -f /etc/rd.conf ]
+  then
+    echo "Configuration already exists. Skipping..." ; echo
+  else
+    sudo wget -O /etc/rd.conf https://7edg.org/rdinstall/rd.conf
+fi
 
 sudo a2enconf rd-bin
 sudo systemctl restart apache2
 
-echo
-echo Creating database...
-echo
+echo ; echo "Creating database and populating database tables..." ; echo
 
-sudo rddbmgr --create --generate-audio
+if [ -d /var/lib/mysql/Rivendell ]
+  then
+    echo "Database already exists. Skipping..."
+  else
+    sudo mysql -e "create database Rivendell;" ; sudo mysql -e "grant Select, Insert, Update, Delete, Create, Drop, References, Index, Alter, Create Temporary Tables, Lock Tables on Rivendell.* to rduser@'%' identified by 'letmein'"
+    sudo rddbmgr --create --generate-audio
+fi
+
 sudo systemctl start rivendell
 sudo systemctl enable rivendell
 
-echo
-echo You may need to reboot your Raspberry Pi to complete the install.
-echo All done. Enjoy.
+echo ; echo "You may need to reboot your Raspberry Pi to complete the install." ; echo All done. Enjoy.
+fi
