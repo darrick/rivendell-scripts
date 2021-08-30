15c41a434d66b1d796c769148c7476aff765cc0a
edgeradio993fm <alastair.ling@gmail.com>
2021-08-30 14:22:28 +1000

    Still working on while loop This could take a while...

--- newupgrade.sh
+++ newupgrade.sh
@@ -45,14 +45,9 @@ while true; do
 read -r -p "Are you sure you want to upgrade your Rivendell install [Y/n] " input
 
 case $input in
-     [yY][eE][sS]|[yY])
-        break ;;
-     [nN][oO]|[nN])
-echo ; echo "${red}Exiting...${reset}" ; echo
-        exit;;
-        *)
-echo "${red}Invalid input...${reset}"
-;;
+     [yY][eE][sS]|[yY]* ) break ;;
+     [nN][oO]|[nN]* ) echo ; echo "${red}Exiting...${reset}" ; echo ; exit;;
+        * ) echo "${red}Invalid input...${reset}";;
 esac
 done
 
